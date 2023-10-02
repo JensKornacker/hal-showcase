6783b721f150cb51890ad5b1075bee7db6e0be06
Jens Kornacker <jens.kornacker@phactum.at>
2023-10-02 19:13:19 +0200

    sidebar and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'hal-showcase';
+  collapedSideBar: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  receiveCollapsed($event: boolean) {
+    this.collapedSideBar = $event;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavigationEnd} from "@angular/router";
+import {SidebarComponent} from "./layout/layout/layout/sidebar/sidebar.component";
+import {NavHeaderComponent} from "./layout/layout/layout/header/nav-header.component";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavHeaderComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+            imports: [
+              BrowserModule,
+              AppRoutingModule,
+              FontAwesomeModule
+            ],
   providers: [],
   bootstrap: [AppComponent]
 })

8035c30933135a2bcffc3fce655d382571592593
Jens Kornacker <jens.kornacker@phactum.at>
2023-10-16 09:24:05 +0200

    navigate Service

--- src/app/task/task-list/task-list.component.ts
+++ src/app/task/task-list/task-list.component.ts
@@ -1,27 +1,16 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
-import {TaskDto} from "../task-dto";
 import {ReactComponentDirective} from "../../shared/react-component.directive";
 import {CommonModule} from "@angular/common";
-import {ListOfTasks, OpenTaskFunction} from "@vanillabp/bc-ui";
-import {
-  Configuration as OfficialApiConfiguration,
-  OfficialTasklistApi,
-  UserTask
-} from "@vanillabp/bc-official-gui-client";
+import {ListOfTasks} from "@vanillabp/bc-ui";
+import {Configuration as OfficialApiConfiguration, OfficialTasklistApi} from "@vanillabp/bc-official-gui-client";
 import {ComponentProps} from "react";
-import {Router} from "@angular/router";
+import {NavigateService} from "../../shared/navigate.service";
 
 export interface ITaskRequest {
   state: string;
   assigned?: boolean;
 }
-
-interface KnownWindowsOpened {
-  [key: string]: Window;
-}
-
-const windowsOpened: KnownWindowsOpened = {};
 
 const translations = {
   "title.long": 'Aufgaben',
@@ -45,8 +34,6 @@ const translations = {
 export class TaskListComponent implements OnInit, OnDestroy {
 
   private subscription$: Subscription[] = [];
-  private taskRequest: ITaskRequest | undefined
-  taskList: TaskDto[] | undefined;
   ListOfTasks = ListOfTasks;
 
   listProps: ComponentProps<typeof ListOfTasks> = {
@@ -55,7 +42,7 @@ export class TaskListComponent implements OnInit, OnDestroy {
     toast: (toast) => {
     },
     openTask: (userTask) => {
-      this.openTask(userTask)
+      this.navigateService.openTask(userTask)
     },
     navigateToWorkflow: (userTask) => {
     },
@@ -66,8 +53,7 @@ export class TaskListComponent implements OnInit, OnDestroy {
   }
 
   constructor(
-    // private taskService: TaskService
-    private router: Router
+    private navigateService: NavigateService
   ) {
   }
 
@@ -78,51 +64,8 @@ export class TaskListComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    // this.getTaskList();
   }
 
-  openTask(userTask: UserTask) {
-    console.log("OPEN TASK", userTask);
-    // this.router.navigate(['tasks', userTask.id])
 
-    // const openTask: OpenTaskFunction = (
-    //   userTask: UserTask,
-    // ) => {
-    let previousWindow: Window | undefined = windowsOpened[userTask.id];
-    if (previousWindow !== undefined) {
-      if (!previousWindow.closed) {
-        previousWindow.focus();
-        return;
-      }
-      delete windowsOpened[userTask.id];
-      previousWindow = undefined;
-    }
-
-    const targetWindowName = `usertask-app-${userTask.id}`;
-    const targetUrl = `/tasks/${userTask.id}`;
-    const targetWindow = window.open(targetUrl, targetWindowName);
-    if (targetWindow) {
-      windowsOpened[userTask.id] = targetWindow;
-      targetWindow.focus();
-    }
-  };
-
-  // getTaskList(): void {
-  //   this.taskRequest = {
-  //     assigned: false,
-  //     state: "CREATED"
-  //   }
-  //   this.subscription$.push(
-  //     this.taskService.getTaskList(this.taskRequest).subscribe({
-  //       next: (result) => {
-  //         this.taskList = result;
-  //         console.log(this.taskList);
-  //       },
-  //       error: err => {
-  //         console.log('error', err);
-  //       }
-  //     })
-  //   )
-  // }
 
 }
